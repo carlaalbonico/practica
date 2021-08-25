c8313a67a31a3c16e5f7eab7f00114ef6093780d
carla albonico <caralbonico@gmail.com>
2021-08-25 19:11:11 -0300

    sigo con html

---
+++ consultarSocio.js
@@ -0,0 +1,16 @@
+//agrega funcion load a HTML; 
+addEventListener("load",load)
+ 
+//variable del servidor
+var miBackEnd = '/practica/serv/';
+
+//DOM
+function $(nombre)
+{
+    return document.getElementById(nombre)
+}
+
+
+function load(){
+   
+}
