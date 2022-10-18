7f360895729db0c6d8e5d9ce33559a556feedc72
carla albonico <caralbonico@gmail.com>
2022-10-18 01:25:50 -0300

    pagina actividades

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function load(){
    
 
     document.getElementById("logIn").addEventListener("click",registrar);
+    document.getElementById("home").addEventListener("click",home);
+    document.getElementById("activities").addEventListener("click",activities);
+    document.getElementById("schedule").addEventListener("click",schedule);
+    document.getElementById("contact").addEventListener("click",contact);
    
 }
 
 function registrar(){
     window.location.assign("http://localhost/practica/login.html");
+    
+}
+function home(){
+    window.location.assign("http://localhost/practica/index.html");
+}
+function activities(){
+    window.location.assign("http://localhost/practica/actividades.html");
+}
+function schedule(){
+    window.location.assign("http://localhost/practica/horarios.html");
+}
+function contact(){
+    window.location.assign("http://localhost/practica/contacto.html");
 }
 
 function TraerFecha(){
