33c0ffea03f3d93d85e59c688fe61de304536b62
carla albonico <caralbonico@gmail.com>
2022-10-26 17:39:06 -0300

    cambios en admin

--- sesionAdmin/sesionAdminPpal.js
+++ sesionAdmin/sesionAdminPpal.js
@@ -1,2 +1,62 @@
 //localhost/practica/sesionAdmin/sesionAdminPpal.html
 
+addEventListener("load",load)
+ 
+//variable del servidor
+var miBackEnd = 'http://localhost:555/';
+var extra= 0; 
+//DOM
+function $(nombre)
+{
+    return document.getElementById(nombre);
+}
+
+
+function load(){
+    //boton para cerrar sesion 
+    document.getElementById("logOut").addEventListener("click",cerrarSesion);
+    //boton para perfil usuario logueado
+    document.getElementById("perfil").addEventListener("click",mostrarPerfil);
+
+    oculta('clases');
+    oculta('formularioModificarClase'); 
+
+
+}
+
+
+
+function cerrarSesion() {
+    sessionStorage.clear();
+    window.location.assign("http://localhost/practica/login.html");
+}
+function mostrarPerfil(){
+    window.location.assign("http://localhost/practica/perfilUsuario.html");
+}
+
+function oculta_muestra(id){
+    if (document.getElementById){ //se obtiene el id
+    var el = document.getElementById(id); 
+    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; 
+    
+    }
+
+}
+
+function muestra(id){
+    if (document.getElementById){ //se obtiene el id
+    var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+
+     el.style.display = "block"; 
+        
+    }
+
+}
+function oculta(id){
+    if (document.getElementById){ //se obtiene el id
+    var el = document.getElementById(id); 
+    el.style.display ='none'; 
+    
+    }
+
+}
