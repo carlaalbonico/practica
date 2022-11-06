f261fc9acdfedea47e6851fca77fa194038b3243
carla albonico <caralbonico@gmail.com>
2022-11-06 12:02:08 -0300

    actividad

--- actividad/menuAdminActividad.js
+++ actividad/menuAdminActividad.js
@@ -12,11 +12,10 @@ function $(nombre)
 
 
 function load(){
+    clickConsultar();
     oculta('botonAtras');
-    oculta('clasesPorProf');
-    oculta('formularioModificarClase'); 
-    oculta('cartel');
     
+    oculta('cartel'); 
     //boton para cerrar sesion 
     document.getElementById("logOut").addEventListener("click",cerrarSesion);
     //boton para perfil usuario logueado
@@ -24,13 +23,10 @@ function load(){
 
     document.getElementById("botonAtras").addEventListener("click",atras);
 
-    document.getElementById("btnRegistrarClasePorProfesor").addEventListener("click",clickConsultarClasePorProf);
-    document.getElementById ('slctDatosClasesxProf').addEventListener('change',validacionClaseClasesxProf);
-    document.getElementById ('slctTipoClase').addEventListener('change',validacionClase); 
+   
     
-    document.getElementById ('tableClases').addEventListener('click',clickModifClase); 
-    document.getElementById('btnGuardarClase').addEventListener("click",click);
-    
+    //document.getElementById('btnGuardar').addEventListener("click",click);
+   
 }
 
 function cerrarSesion() {
@@ -69,289 +65,136 @@ function oculta(id){
 }
 function atras(){ 
     oculta('botonAtras');
-    
-    muestra('clases'); 
-    oculta('clasesPorProf'); 
-    oculta('formularioModificarClase'); 
-    oculta('cartel'); 
-
-}
-function clickRegistrarClase(){
-    window.location.assign("http://localhost/practica/clase/registrarActividad.html");//aca va el enlace de la pagina registrar; 
-}
-
-function clickConsultarClase(){
-    muestra('botonAtras');
-    
-    oculta('cartel'); 
-
-    enviarParametrosGET(miBackEnd + 'Actividad',cargarOpcionesClase); 
-    enviarParametrosGET(miBackEnd + 'Clase', retornoDelClickConsultarClase);
-    
-}
-
-function validacionClase(){
-    enviarParametrosGET(miBackEnd + 'Clase', retornoDelClickConsultarClase);
-}
-function cargarOpcionesClase(nro){
-    var clase= JSON.parse(nro);
-    console.log(clase);
-    clase.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
-
-    var opciones = ['<option value="0">Todas</option>']
-
-   clase.forEach(element => {
-        opciones.push('<option value="' + element.nombre + '">' + element.nombre + '</option>');
-    });
-    console.log(opciones); 
-    $("slctTipoClase").innerHTML = opciones;    
-    
-}
-
-function retornoDelClickConsultarClase(valor){
-    var tipoClase= document.getElementById("slctTipoClase").value; 
-    var clases =JSON.parse(valor);
    
-        if (tipoClase!=0){
-            
+    muestra('actividad');
+   
+    oculta('cartel'); 
 
-            clases.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
-            var clasesFiltradas= clases.filter( item =>{
-                var nombreMin= item.nombre.toLowerCase();
-                return nombreMin.includes(tipoClase.toLowerCase()); 
+}
+function clickRegistrarRutina(){
+    window.location.assign("http://localhost/practica/rutina/registrarRutina.html");//aca va el enlace de la pagina registrar; 
+}
+
+function clickConsultar(){
+    oculta('botonAtras');
+   
+    muestra('actividad');
+    
+    oculta('cartel'); 
+    cargarSkeletonTabla();
+    enviarParametrosGET(miBackEnd + 'Actividad',retornoDelClickConsultar);
+    
+}
+function cargarSkeletonTabla(){
+    var opciones = [];
+
+    for(let i=0; i < 5; i++){
+        opciones.push(
+            '<tr>' +
+                '<td><p id="skeletonTabla">' + "-" + '</p></td>' +
+                '<td><p id="skeletonTabla">' + "-" + '</p></td>' +
+                '<td><p id="skeletonTabla">' + "-" + '</p></td>' +
                 
-            }); 
+        
+            '</tr>'
+        );
+    }
 
-            var opciones=[]; 
+    $('tableActividad').innerHTML = opciones.join('');
+    
+}
 
 
-            clasesFiltradas.forEach(element => {
+
+function retornoDelClickConsultar(valor){
+    
+     actividad =JSON.parse(valor);
+    opciones=[]; 
+            actividad.forEach(element => {
                 opciones.push('<tr >'+
-                '<th scope="row">'+element.idClase+'</th>'+
+                '<th scope="row">'+element.idActividad+'</th>'+
                 '<td>'+element.nombre+'</td>'+
-                '<td>'+element.dias+'</td>'+
-                '<td>'+element.horaDeInicio+'</td>'+
-                '<td>'+element.horaDeFin+'</td>'+
-                '<td>'+element.salon+'</td>'+
-                '<td>'+element.profesor+'</td>'+
-                '<td>'+element.cupos+'</td>'+
                 
-                '<td><button class="btn  btn-danger modificacion" type="button" id="'+element.idClase+'">Modificar</button></td>'+
-
+             
+                '<td><button class="btn  btn-danger modificacion" type="button" onclick="clickModif('+element.idActividad+')">Modificar</button></td>'+
+            
                 '</tr>' );
                 
             });
 
-
-            
-        $('tableClases').innerHTML=opciones; 
-                    
-
-    }else{
-        
-        var opciones=[]; 
-
-        clases.forEach(element => {
-            opciones.push('<tr >'+
-            '<th scope="row">'+element.idClase+'</th>'+
-            '<td>'+element.nombre+'</td>'+
-            '<td>'+element.dias+'</td>'+
-            '<td>'+element.horaDeInicio+'</td>'+
-            '<td>'+element.horaDeFin+'</td>'+
-            '<td>'+element.salon+'</td>'+
-            '<td>'+element.profesor+'</td>'+
-            '<td>'+element.cupos+'</td>'+
-           
-            '<td><button class="btn btn-danger modificacion"  id="'+element.idClase+'">Modificar</button></td>'+
-
-            '</tr>' );
-            
-        });
-        $('tableClases').innerHTML=opciones; 
-    }
+            $('tableActividad').innerHTML = opciones.join('');
+ 
 }
 
-function clickModifClase(){
-    
-    let modificacion = document.querySelectorAll(".modificacion"); 
-    var idDeBoton=0; 
-    modificacion.forEach((boton) => {
-      boton.addEventListener("click", function(e){
-        e.preventDefault();
-        
-        console.log(boton.id); 
-        idDeBoton= boton.id; 
-        extra=  idDeBoton;
-       
-        if (idDeBoton!=0){
-            if(confirm('¿Esta seguro que desea modificar esta clase?')){
-            validar(); 
-            }}      
-       return  idDeBoton; 
-      })
-    });
+function clickModif(id){
     
     
+            
+            swal({
+                title: "Modificar",
+                text: "¿Esta seguro que desea modificar esta actividad?",
+                icon: "warning",
+                buttons: true,
+                dangerMode: true,
+              })
+              .then((willDelete) => {
+                if (willDelete) {
+
+                    extra= idRutina;
+                    validar(); 
+                } 
+              });
+  
 
 }
+
 
 function validar(){
     muestra('botonAtras');
+    oculta('actividad'); 
     
-    muestra('clases'); 
-    oculta('clasesPorProf'); 
-    muestra('formularioModificarClase'); 
-    oculta('cartel'); 
-    enviarParametrosGET(miBackEnd + 'Clase', retornoDelClickModificarClase);
-    enviarParametrosGET(miBackEnd + 'Actividad',cargarOpcionesClaseMod);
-    enviarParametrosGET(miBackEnd + 'Profesor',cargarOpcionesProf);
-    enviarParametrosGET(miBackEnd + 'Salon',cargarOpcionesSalon);
-}
-function cargarOpcionesClaseMod(nro){
-    var clase= JSON.parse(nro);
-    console.log(clase);
-    clase.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
-
-    var opciones = []
-
-   clase.forEach(element => {
-        opciones.push('<option value="' + element.idActividad + '">' + element.nombre + '</option>');
-    });
+    oculta('cartel');
+    enviarParametrosGET(miBackEnd + 'Actividad', retornoDelClickModificarRutina);
     
-    $("slctClase").innerHTML = opciones; 
 }
 
-function cargarOpcionesProf(nroProf){
+
+
+
+    function retornoDelClickModificar(valor){
+
+        var actividad =JSON.parse(valor);
+        var id;  
+        var nombre;
+            
     
-    var profes= JSON.parse(nroProf);
-    console.log(profes);
-    profes.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
-
-    var opciones = []
-
-    profes.forEach(element => {
-        opciones.push('<option value="' + element.legajo + '">' + element.nombre +' '+ element.apellido + '</option>');
-    });
-    
-    $("slctDatosProf").innerHTML = opciones;
-    
-   
-}
-function cargarOpcionesSalon(nro){
-    
-    var salon= JSON.parse(nro);
-    console.log(salon);
-    salon.sort(function (x, y) { return x.nombreSalon.localeCompare(y.nombreSalon) });
-
-    var opciones = []
-
-    salon.forEach(element => {
-        opciones.push('<option value="' + element.idSalon + '">' + element.nombreSalon +' capacidad:'+ element.capacidad + '</option>');
-    });
-    
-    $("slctDatosSalon").innerHTML = opciones;
-
-   
-}
-function retornoDelClickModificarClase(valor){
-
-    var clases =JSON.parse(valor);
-        var dia ;
-        var horaInicio;
-        var horaFin;
-      
-    console.log(clases); 
-
-     clases.forEach(element => {
-        if(element.idClase==extra){
-           console.log(element.dias); 
-           console.log(element.horaDeInicio); 
-           console.log(element.horaDeFin); 
-           console.log(element.profesor); 
-           console.log(element.salon); 
-        dia = element.dias;
-        horaInicio = element.horaDeInicio;
-        horaFin= element.horaDeFin;
-        profesor= element.profesor;
-        salon = element.salon;
+        console.log(actividad); 
+         
+            actividad.forEach(element => {
+            if(element.idActividad==extra){
+            id=element.idActividad;
+            nombre = element.nombre;
+             
+            }
+            
+        });
        
-        }
-        
-    });
-   
-        $("slctDias").value = dia;
-        $("txtHoraInicio").value = horaInicio;
-        $("txtHoraFin").value = horaFin;
-        
-}
-
+            $("txtNombre").value = nombre;
+            
+    }
+    
 function click(){
-    // $("btnGuardarClase").disabled=true;
-    alert(extra);
-     enviarInfoDeClase(miBackEnd + 'Clase/Actualizacion'+extra, respuestaDeServidor);
- }
- 
- function respuestaDeServidor(respuesta){
-     muestra('cartel');
-     $("respuesta").innerHTML=respuesta;
-    
- }
-
-function clickConsultarClasePorProf(){
-    muestra('botonAtras');
-    
-    oculta('clases'); 
-    muestra('clasesPorProf'); 
-    oculta('formularioModificarClase'); 
-    oculta('cartel'); 
-
-    enviarParametrosGET(miBackEnd + 'Profesor',cargarOpcionesClasesxProf);
-
-
-
-
+  
+         enviarInfo(miBackEnd + 'Actividad'+extra, respuestaDeServidor);
 }
-function cargarOpcionesClasesxProf(nroProf){
-    
-    var profes= JSON.parse(nroProf);
-    console.log(profes);
-    profes.sort(function (x, y) { return x.nombre.localeCompare(y.nombre) });
-
-    var opciones = []
-
-    profes.forEach(element => {
-        opciones.push('<option value="' + element.legajo + '">' + element.nombre +' '+ element.apellido + '</option>');
-    });
-    
-    $("slctDatosClasesxProf").innerHTML = opciones;
-    
-}
-function validacionClaseClasesxProf(){
-    var legajo= document.getElementById("slctDatosClasesxProf").value; 
-    console.log(legajo);
-    enviarParametrosGET(miBackEnd + 'Clase/Profesor/'+legajo,  cargarOpcionesClasesPorProf);
-}
-
-function cargarOpcionesClasesPorProf(valor){
-    console.log(valor);
-    var clases =JSON.parse(valor);
-
-    console.log(clases);
-    var opciones=[]; 
-
-    clases.forEach(element => {
-        opciones.push('<tr >'+
-        '<th scope="row">'+element.idClase+'</th>'+
-        '<td>'+element.dias+'</td>'+
-        '<td>'+element.horaDeInicio+'</td>'+
-        '<td>'+element.horaDeFin+'</td>'+
-        '<td>'+element.salon+'</td>'+
-        '</tr>' );
+     
+function respuestaDeServidor(respuesta){
+        swal("Genial!", '"'+respuesta+'"', "success");
+        clickConsultarRutina();
         
-    });
-    $('tableClasesPorProf').innerHTML=opciones; 
-
 }
+
+
 function enviarParametrosGET(servidor,funcionARealizar){
 
     //Declaro el objeto
@@ -377,58 +220,16 @@ function enviarParametrosGET(servidor,funcionARealizar){
     xmlhttp.send();
 }
 
-function enviarParametrosPOSTEsp(servidor, funcionARealizar){
+function enviarInfo(servidor, funcionARealizar){
 
     //declaro el objeto
     var xmlhttp = new XMLHttpRequest(); 
 
     //agrega datos para pasar por POST
     var datos = new FormData();
-    datos.append('especialidad',$("txtEmail").value);
+    datos.append("nombre",$("txtNombre").value);
    
-
-    //indico hacia donde va el mensaje
-    xmlhttp.open ("POST", servidor, true); 
-
-    //seteo el evento
-    xmlhttp.onreadystatechange = function(){
-        //veo si llego la respuesta del servidor
-        if(xmlhttp.readyState==XMLHttpRequest.DONE){
-            //reviso si la respuesta del servidor es la correcta
-            if(xmlhttp.status==200){
-                funcionARealizar(xmlhttp.response);
-            }else{
-                swal("Error", "revise los datos cargados", "error");
-            };
-        }
-    }
-    //esto va siempre cuando se hace un formulario
-    xmlhttp.setRequestHeader("enctype","multipart/form-data");
-
-    //envio el mensaje 
-    xmlhttp.send(datos);
-
-
-}
-function enviarInfoDeClase(servidor, funcionARealizar){
-
-    //declaro el objeto
-    var xmlhttp = new XMLHttpRequest(); 
-
-    //agrega datos para pasar por POST
-    var datos = new FormData();
-    datos.append("idclase",extra);
-    datos.append("dias",$("slctDias").value);
-    datos.append("horaDeInicio",$("txtHoraInicio").value);
-    datos.append("horaDeFin",$("txtHoraFin").value);
-    datos.append("fechaDeInicio",$("txtFechaInicio").value);
-    datos.append("fechaDeFin",$("txtFechaFin").value);
-    datos.append("cupos",$("txtCupos").value);
-    datos.append("profesor",$("slctDatosProf").value);
-    datos.append("salon",$("slctDatosSalon").value);
-    datos.append("modalidad",$("slctMod").value);
         //indico hacia donde va el mensaje
-        
     xmlhttp.open ("POST", servidor, true); 
 
     //seteo el evento
